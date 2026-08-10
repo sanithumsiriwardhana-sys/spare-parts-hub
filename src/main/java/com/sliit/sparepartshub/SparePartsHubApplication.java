53406d9ebcf7733b3dceb899e4c6739d4cceeb61
Sanithu.S <it25101412@my.sliit.lk>
2026-08-10 18:08:27 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/sliit/sparepartshub/SparePartsHubApplication.java
@@ -0,0 +1,13 @@
+package com.sliit.sparepartshub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparePartsHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SparePartsHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sliit/sparepartshub/SparePartsHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sliit.sparepartshub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparePartsHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
